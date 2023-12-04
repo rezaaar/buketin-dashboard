0365de4b49e20812e09fc8887dd037680870f673
rezaaar <rabbs3245@gmail.com>
2023-12-04 20:22:02 +0700

    Add start script to package.json and update product page layout

--- pages/products.vue
+++ pages/products.vue
@@ -41,13 +41,24 @@
                 </UFormGroup>
 
                 <UFormGroup label="Product category" name="category">
-                  <USelect v-model="state.category" :options="categories" option-attribute="name">
+                  <USelect v-model="state.category" :options="categories" option-attribute="name" value-attribute="_id">
                   </USelect>
                 </UFormGroup>
 
                 <UFormGroup label="Product occasion" name="occasion">
                   <div class="grid grid-cols-3 gap-2">
-                    <UCheckbox v-model="state.occasion" v-for="occasion in occasions" :label="occasion.name" />
+                    <!-- <input type="checkbox"> -->
+                    <div class="relative flex items-start" v-for="occasion in occasions">
+                      <div class="flex items-center h-5">
+                        <input :id="occasion._id" type="checkbox" v-model="selectedOccasion"
+                          class="form-checkbox h-4 w-4 dark:checked:bg-current dark:checked:border-transparent dark:indeterminate:bg-current dark:indeterminate:border-transparent disabled:opacity-50 disabled:cursor-not-allowed focus:ring-0 focus:ring-transparent focus:ring-offset-transparent rounded bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-700 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 focus-visible:ring-offset-2 focus-visible:ring-offset-white dark:focus-visible:ring-offset-gray-900 text-primary-500 dark:text-primary-400"
+                          :value="occasion._id">
+                      </div>
+                      <div class="ms-3 flex flex-col">
+                        <label :for="occasion._id" class="text-sm font-medium text-gray-700 dark:text-gray-200">{{
+                          occasion.name }}</label>
+                      </div>
+                    </div>
                   </div>
                 </UFormGroup>
 
@@ -69,10 +80,28 @@
 
       <div class="mt-4">
         <UTable :rows="products" :columns="columns">
+
+          <template #category-data="{ row }">
+            <div>
+              {{ row.category ? row.category.name : row.category }}
+            </div>
+          </template>
+
+          <template #occasion-data="{ row }">
+            <div>
+              {{ row.occasion.map(el => el.name).toString() }}
+            </div>
+          </template>
+
+          <template #image-data="{ row }">
+            <div>
+              <img :src="`http://localhost:3030//images/${row.image}`" alt="" class="w-20 h-20 object-cover rounded-md">
+            </div>
+          </template>
+
           <template #actions-data="{ row }">
             <div class="flex space-x-4">
-              <UButton color="red" variant="ghost" icon="i-heroicons-trash" label="Delete" />
-              <UButton color="sky" variant="ghost" icon="i-heroicons-adjustments-vertical" label="Update" />
+              <UButton color="red" variant="ghost" icon="i-heroicons-trash" label="Delete" @click="deleteProduct(row._id)"/>
             </div>
           </template>
         </UTable>
@@ -85,23 +114,18 @@
 const products = ref([])
 const categories = ref([])
 const occasions = ref([])
+const selectedOccasion = ref([])
+const uploadedImage = ref(null)
 const state = reactive({
   name: undefined,
   price: undefined,
   description: undefined,
   category: undefined,
-  occasion: undefined,
-  image: undefined
 })
 
 const file = ref(null);
-
-const fileName = computed(() => file.value?.name);
-const fileExtension = computed(() => fileName.value?.substr(fileName.value?.lastIndexOf(".") + 1));
-const fileMimeType = computed(() => file.value?.type);
-
-
 const isModalCreateOpen = ref(false)
+const config = useRuntimeConfig()
 
 const columns = [
   {
@@ -113,16 +137,16 @@ const columns = [
     label: 'Price'
   },
   {
-    key: 'stock',
-    label: 'Stock'
-  },
-  {
     key: 'category',
     label: 'Category'
   },
   {
     key: 'occasion',
     label: 'Occasion'
+  },
+  {
+    key: 'image',
+    label: 'Image'
   },
   {
     key: 'actions',
@@ -152,40 +176,30 @@ const { data: dataOccasion } = await useGetFetch('occasion', {
 
 const uploadFile = (event) => {
   file.value = event.target.files[0];
-
   submitFile();
 };
 
 const submitFile = async () => {
-  // const reader = new FileReader();
-  // reader.readAsDataURL(file.value);
-  // reader.onload = async () => {
-  //   const encodedFile = reader.result.split(",")[1];
-  //   const data = {
-  //     file: encodedFile,
-  //     fileName: fileName.value,
-  //     fileExtension: fileExtension.value,
-  //     fileMimeType: fileMimeType.value,
-  //   };
+  const formData = new FormData()
+  formData.append("file", file.value)
 
-    const formData = new FormData()
-    formData.append("file", file.value)
+  // console.log(formData.get(file));
+  try {
+    const uploadEndpoint = "http://localhost:3030/upload/one";
+    // const response = await axios.post(endpoint, data);
 
-    // console.log(formData.get(file));
-    try {
-      const endpoint = "http://localhost:3030/upload/one";
-      // const response = await axios.post(endpoint, data);
+    const { data: dataImage } = await useFetch(uploadEndpoint, {
+      method: 'POST',
+      body: formData,
 
-      const {data: dataImage} = await useFetch(endpoint, {
-        method: 'POST',
-        body: formData
-      })
-
-      console.log(dataImage.value);
-    } catch (error) {
-      console.error(error);
-    }
-  // };
+      onResponse({ response }) {
+        uploadedImage.value = response._data.filename
+        console.log(uploadedImage.value);
+      }
+    })
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 
@@ -195,14 +209,32 @@ const validate = (state) => {
   if (!state.price) errors.push({ path: 'price', message: 'Required' })
   if (!state.description) errors.push({ path: 'description', message: 'Required' })
   if (!state.category) errors.push({ path: 'category', message: 'Required' })
-  if (!state.occasion) errors.push({ path: 'occasion', message: 'Required' })
-  if (!state.image) errors.push({ path: 'image', message: 'Required' })
+  if (!selectedOccasion.value) errors.push({ path: 'occasion', message: 'Required' })
+  if (!uploadedImage.value) errors.push({ path: 'image', message: 'Required' })
   return errors
 }
 
 async function onSubmit(event) {
   // Do something with data
-  console.log(event.data)
+  const { data: dataProduct } = useFetch('product', {
+    baseURL: config.public.API_BASE,
+    method: 'POST',
+    body: {
+      name: state.name,
+      price: parseInt(state.price, 10),
+      description: state.description,
+      category: state.category,
+      occasion: selectedOccasion.value,
+      image: uploadedImage.value
+    }
+  });
+}
+
+const deleteProduct = (id) => {
+  useFetch(`product/${id}`, {
+    baseURL: config.public.API_BASE,
+    method: 'DELETE',
+  })
 }
 
 
